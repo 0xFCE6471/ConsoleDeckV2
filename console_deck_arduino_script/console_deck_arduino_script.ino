ee127798923bb8229d91678def3b7ddd0ec9f3af
0xKEYSEX <104474401+0xFCE6471@users.noreply.github.com>
2025-10-26 13:52:52 -0300

    Update console_deck_arduino_script.ino

--- console_deck_arduino_script/console_deck_arduino_script.ino
+++ console_deck_arduino_script/console_deck_arduino_script.ino
@@ -1,26 +1,58 @@
-const int numPulsanti = 9;
-const int pinPulsanti[numPulsanti] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
-bool statoPrecedente[numPulsanti] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
+#define CLK 5
+#define DT 4
+#define SW 3
+
+const int buttonPins[] = {A1, A2, A0, 11, 10, 9, 2, 6, 7, 8};  // ultimi = MEDIA
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 0; i < numPulsanti; i++) {
-    pinMode(pinPulsanti[i], INPUT_PULLUP);
+
+  // Encoder
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(SW, INPUT_PULLUP);
+  lastStateCLK = digitalRead(CLK);
+
+  // Pulsanti
+  for (int i = 0; i < 10; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
   }
 }
 
 void loop() {
-  for (int i = 0; i < numPulsanti; i++) {
-    int statoAttuale = digitalRead(pinPulsanti[i]);
-
-    // Rileva il fronte di discesa (quando il pulsante viene premuto)
-    if (statoPrecedente[i] == HIGH && statoAttuale == LOW) {
-      Serial.print("BUTTON_");
-      Serial.println(i + 1);  // Stampa BUTTON_1, BUTTON_2, ...
+  // Encoder rotazione
+  currentStateCLK = digitalRead(CLK);
+  if (currentStateCLK != lastStateCLK) {
+    if (digitalRead(DT) != currentStateCLK) {
+      counter++;
+    } else {
+      counter--;
     }
+    Serial.print("VOLUME_");
+    Serial.println(counter);
+  }
+  lastStateCLK = currentStateCLK;
 
-    statoPrecedente[i] = statoAttuale;
+  // Pulsante encoder = MUTE
+  if (digitalRead(SW) == LOW) {
+    Serial.println("MUTE");
+    delay(200); // debounce
   }
 
-  delay(20);  // debounce software semplice
+  // Pulsanti utente
+  for (int i = 0; i < 10; i++) {
+    if (digitalRead(buttonPins[i]) == LOW) {
+      if (i < 9) {
+        Serial.print("BUTTON_");
+        Serial.println(i + 1);
+      } else {
+        Serial.println("MEDIA");
+      }
+      delay(200); // debounce
+    }
+  }
 }
